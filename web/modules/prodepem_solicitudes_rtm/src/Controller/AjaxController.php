17350ac490a7f8d3c1ba71012a10c1f68e3c00bd
Camilo Cervantes Salazar <cacesa8931@gmail.com>
2026-01-18 17:26:25 -0500

    api metodos de pago

--- web/modules/prodepem_solicitudes_rtm/src/Controller/AjaxController.php
+++ web/modules/prodepem_solicitudes_rtm/src/Controller/AjaxController.php
@@ -172,4 +172,144 @@ class AjaxController extends ControllerBase {
     return new JsonResponse($data);
   }
 
+  /**
+   * Returns data based on payment method and node ID.
+   *
+   * @param int $nid
+   *   The node ID.
+   * @param string $metodo_pago
+   *   The name of the payment method term.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   The JSON response.
+   */
+  public function getDatosMetodoPago($nid, $metodo_pago) {
+    $data = [];
+    $node = \Drupal\node\Entity\Node::load($nid);
+
+    // Replace hyphens with spaces to match taxonomy term names.
+    $metodo_pago = str_replace('-', '', $metodo_pago);
+    $metodo_pago = str_replace(' ', '', $metodo_pago);
+    //uppercase de metodopago
+    $metodo_pago = strtoupper($metodo_pago);
+
+    if ($node) {
+      switch ($metodo_pago) {
+        case 'EFECTIVO':
+          if ($node->hasField('field_puntos_de_venta')) {
+            $paragraphs = $node->get('field_puntos_de_venta')->referencedEntities();
+            foreach ($paragraphs as $paragraph) {
+              $data[] = $this->getEntityData($paragraph);
+            }
+          }
+          break;
+
+        case 'QR':
+          if ($node->hasField('field_qr')) {
+            $data['field_qr'] = $this->getFieldValue($node, 'field_qr');
+          }
+          if ($node->hasField('field_cuentas_bancarias')) {
+            $paragraphs = $node->get('field_cuentas_bancarias')->referencedEntities();
+            foreach ($paragraphs as $paragraph) {
+              $data['field_cuentas_bancarias'][] = $this->getEntityData($paragraph);
+            }
+          }
+          break;
+
+        case 'BRE':
+          if ($node->hasField('field_enlace_bre')) {
+            $data['field_enlace_bre'] = $this->getFieldValue($node, 'field_enlace_bre');
+          }
+          break;
+
+        case 'CONVENIOS':
+          if ($node->hasField('field_convenios')) {
+            $paragraphs = $node->get('field_convenios')->referencedEntities();
+            foreach ($paragraphs as $paragraph) {
+              $data['field_convenios'][] = $this->getEntityData($paragraph);
+            }
+          }
+          break;
+
+        case 'SISTECREDITO':
+          if ($node->hasField('field_enlace_asesor_sistecredito')) {
+            $data['field_enlace_asesor_sistecredito'] = $this->getFieldValue($node, 'field_enlace_asesor_sistecredito');
+          }
+          break;
+
+        case 'CORRESPONSALBANCARIO':
+          if ($node->hasField('field_cuentas_bancarias')) {
+            $paragraphs = $node->get('field_cuentas_bancarias')->referencedEntities();
+            foreach ($paragraphs as $paragraph) {
+              $data['field_cuentas_bancarias'][] = $this->getEntityData($paragraph);
+            }
+          }
+          break;
+      }
+    }
+
+    return new JsonResponse($data);
+  }
+
+  /**
+   * Helper to get all field values from an entity.
+   */
+  private function getEntityData($entity) {
+    $data = [];
+    foreach ($entity->getFieldDefinitions() as $field_name => $definition) {
+      if ($definition->getFieldStorageDefinition()->isBaseField()) {
+        continue;
+      }
+      if (!$entity->get($field_name)->isEmpty()) {
+        $data[$field_name] = $this->getFieldValue($entity, $field_name);
+      }
+    }
+    return $data;
+  }
+
+  /**
+   * Helper to get field value handle different field types.
+   */
+  private function getFieldValue($entity, $field_name) {
+    $field = $entity->get($field_name);
+    $values = [];
+
+    foreach ($field as $item) {
+      $val = $item->getValue();
+      if (isset($val['target_id'])) {
+        // Handle referenced entities (taxonomy terms, media, etc.)
+        $referenced_entity = $item->entity;
+        if ($referenced_entity) {
+          if ($referenced_entity->getEntityTypeId() === 'taxonomy_term') {
+            $values[] = [
+              'tid' => $referenced_entity->id(),
+              'name' => $referenced_entity->label(),
+            ];
+          } elseif ($referenced_entity->getEntityTypeId() === 'file') {
+             $values[] = [
+              'url' => \Drupal::service('file_url_generator')->generateAbsoluteString($referenced_entity->getFileUri()),
+              'name' => $referenced_entity->getFilename(),
+            ];
+          } elseif ($referenced_entity->getEntityTypeId() === 'media') {
+             // Basic media support
+             $values[] = [
+               'mid' => $referenced_entity->id(),
+               'name' => $referenced_entity->label(),
+             ];
+          } else {
+            $values[] = $referenced_entity->id();
+          }
+        }
+      } elseif (isset($val['value'])) {
+        $values[] = $val['value'];
+      } elseif (isset($val['uri'])) {
+        $values[] = $val['uri'];
+      } else {
+        $values[] = $val;
+      }
+    }
+
+    return count($values) === 1 ? reset($values) : $values;
+  }
+
 }
