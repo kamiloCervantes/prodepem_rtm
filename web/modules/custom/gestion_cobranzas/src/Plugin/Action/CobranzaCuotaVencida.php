16e5affa20d62fbae46785146dc31e7f025bd4f8
Camilo Cervantes <cacesa8931@gmail.com>
2026-02-13 17:47:22 -0500

    notifications actions

---
+++ web/modules/custom/gestion_cobranzas/src/Plugin/Action/CobranzaCuotaVencida.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\gestion_cobranzas\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\webform\Entity\WebformSubmission;
+
+/**
+ * Registra un submission en el webform notificaciones_cobranzas.
+ *
+ * @Action(
+ *   id = "gestion_cobranzas_cobranza_cuota_vencida",
+ *   label = @Translation("Cobranza cuota vencida"),
+ *   type = "node"
+ * )
+ */
+class CobranzaCuotaVencida extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    if (!$entity) {
+      return;
+    }
+
+    $timestamp = $entity->get('field_fecha_vencimiento_tipado')->value;
+    $mes = 'N/A';
+    if ($timestamp) {
+      $meses = [
+        1 => 'enero', 2 => 'febrero', 3 => 'marzo', 4 => 'abril',
+        5 => 'mayo', 6 => 'junio', 7 => 'julio', 8 => 'agosto',
+        9 => 'septiembre', 10 => 'octubre', 11 => 'noviembre', 12 => 'diciembre'
+      ];
+      $mes = $meses[(int) date('n', $timestamp)] ?? 'N/A';
+    }
+
+    $values = [
+      'webform_id' => 'notificaciones_cobranzas',
+      'data' => [
+        'mensaje' => 'La cuota de su factura del mes de ' . $mes . ' ha vencido.',
+        'destinatario' => $entity->get('field_id_cliente')->value ?? '',
+        'enviado' => 0,
+      ],
+    ];
+
+    /** @var \Drupal\webform\WebformSubmissionInterface $webform_submission */
+    $webform_submission = WebformSubmission::create($values);
+    $webform_submission->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    /** @var \Drupal\node\NodeInterface $object */
+    return $object->access('update', $account, $return_as_object);
+  }
+
+}
